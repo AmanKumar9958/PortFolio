30e95034c76301d5686229cdb7708befb896de06
Aman Kumar <151604983+AmanKumar9958@users.noreply.github.com>
2024-02-07 09:41:24 +0000

    2 more C projects added

---
+++ Projects/arrayAdditon.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+int main()
+{
+    int a[3][3], b[3][3], c[3][3], d[3][3], i, j, k;
+    printf("Enter the element of matrix A: ");
+    for (i=0; i<3; i++)
+        {
+            for(j=0; j<3; j++)
+                {
+                    scanf("%d", &a[i][j]);
+                }
+        }
+    printf("Enter the element of matrix B: ");
+    for (i=0; i<3; i++)
+        {
+            for(j=0; j<3; j++)
+                {
+                    scanf("%d", &b[i][j]);
+                }
+        }
+    //FOR ADDITION
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            c[i][j]= a[i][j]+b[i][j];
+        }
+        
+    }
+    printf("Addition of two matrices is\n");
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            printf("%d\t", c[i][j]);
+        }
+        
+    printf("\n");
+    }
+    
+    // getch();
+    return 0;
+    
+}
